76c739c668a4f784137351297fcb3510493ff76f
plusbeauxjours <plusbeauxjours@gmail.com>
2019-07-10 15:41:19 +0900

    creating the store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {
+    injectGlobal
+} from "styled-components";
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import reset from 'styled-reset';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+injectGlobal `
+${reset};
+body{
+    background-color: #ecf0f1
+}
+`
+ReactDOM.render( < App / > , document.getElementById('root'));
